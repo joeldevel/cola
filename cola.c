36bebb8c17e449daf35adc33b7113474346e8566
joeldevel <christianjoel.developer@gmail.com>
2020-11-15 19:10:26 -0300

    last submit, todo oK

--- cola.c
+++ cola.c
@@ -1,191 +1,72 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include "cola.h"
-// el nodo ?
+
 typedef struct nodo {
     void *dato;
     struct nodo * siguiente_nodo;
 } nodo_t;
-// el struct
+
 typedef struct cola {
-    nodo_t* primero_en_entrar;
-    nodo_t* ultimo_en_entrar;
+    nodo_t* primero;
+    nodo_t* ultimo;
 } cola_t;
-// primitivas para practicar
+
+nodo_t* nodo_crear(void * valor) {
+    nodo_t *nodo = malloc(sizeof(nodo_t));
+    if (!nodo) return NULL;
+    nodo->dato = valor;
+    nodo->siguiente_nodo = NULL;
+    return nodo;
+}
+
 cola_t* cola_crear(void) {
     cola_t *cola = malloc(sizeof(cola_t));
     if (!cola) return NULL;
-    cola->primero_en_entrar = NULL;
-    cola->ultimo_en_entrar = NULL;
+    cola->primero = NULL;
+    cola->ultimo = NULL;
     return cola;
 }
+
 bool cola_esta_vacia(const cola_t *cola) {
-    return cola->ultimo_en_entrar == NULL && cola->primero_en_entrar == NULL;
+    return cola->ultimo == NULL;
 }
 
-// void cola_esta_vacia(cola_t *cola) {
-//     if (vacia(cola)) printf("esta vacia\n");
-//     else printf("No esta vacia\n");
-// }
-
 bool cola_encolar(cola_t *cola, void *valor) {
-    nodo_t *nodo = malloc(sizeof(nodo_t));
+    nodo_t *nodo = nodo_crear(valor);
     if (!nodo) return false;
-    nodo->dato = valor;
-    nodo->siguiente_nodo = NULL;
-    // cola vacia
-    if (cola->ultimo_en_entrar == NULL && cola->primero_en_entrar == NULL) {
-        cola->ultimo_en_entrar = nodo;
-        cola->primero_en_entrar = nodo;
-        return true;
+    if (cola_esta_vacia(cola)) {
+        cola->primero = nodo;
+    } else {
+        cola->ultimo->siguiente_nodo = nodo;
     }
-    cola->ultimo_en_entrar->siguiente_nodo = nodo;
-    cola->ultimo_en_entrar = cola->ultimo_en_entrar->siguiente_nodo;
+    cola->ultimo = nodo;
     return true;
 }
 
 void *cola_ver_primero(const cola_t *cola) {
-    return cola->primero_en_entrar->dato;
+    return (cola_esta_vacia(cola))? NULL: cola->primero->dato;
 }
 
 void *cola_desencolar(cola_t *cola) {
-    // printf("(Entrando a cola_desencolar)\n" );
-
     if (cola_esta_vacia(cola)) return NULL;
-
-    nodo_t * tmp = cola->primero_en_entrar;
-    // 1 solo elemento
-    if (cola->primero_en_entrar == cola->ultimo_en_entrar) {
-        cola->ultimo_en_entrar = NULL;
-        cola->primero_en_entrar = NULL;
-        return &tmp->dato;
-    }
-    cola->primero_en_entrar = cola->primero_en_entrar->siguiente_nodo;
-    // free(tmp);
-    // printf("(cola_desencolar) despues del free\n" );
-    // if (cola->primero_en_entrar == NULL) {
-    //     printf("cola->primero_en_entrar=NULL\n");
-    //     return NULL;
-    // }
-    return &tmp->dato;
-}
-void cola_destruir(cola_t *cola, void (*destruir_dato)(void *)) {
-    // printf("(1 Entrando a cola_destruir)\n" );
-    if (destruir_dato == NULL) {
-        while (cola->primero_en_entrar) {
-            void *tmp = cola_desencolar(cola);
-            free(tmp);
-            // cola_desencolar(cola);
-            // printf("(dentro del while)\n" );
-        }
+    nodo_t *tmp = cola->primero;
+    void *p = tmp->dato;
+    if (cola->primero == cola->ultimo) {
+        cola->ultimo = NULL;
     }
     else {
-        while (cola->primero_en_entrar) {
-            // printf("2 destruyendo: %d\n",*(int*)cola_ver_primero(cola) );
-            destruir_dato(cola->primero_en_entrar->dato);
-            // printf("( 3 dentro del while del else)\n" );
-            // cola_desencolar(cola);
-            void *tmp = cola_desencolar(cola);
-            free(tmp);
-        }
+        cola->primero = cola->primero->siguiente_nodo;
     }
-    // free(cola->primero_en_entrar);
-    // free(cola->ultimo_en_entrar);
-    // printf("antes de free(cola)\n");
-    // printf("cola_ver_primero: %d\n",*(int*)cola_ver_primero(cola) );
-
-    free(cola);
-
-}
-/*
-int main(int argc, char *argv[]) {
-    cola_t *cola = crear_cola();
-    // esta_vacia(cola);
-    // encolar( cola, 87);
-    esta_vacia(cola);
-    // int datito = 34;
-    // int datito2 = 495;
-    // cola_encolar( cola, &datito);
-    // cola_encolar( cola, &datito2);
-    // test con datos en memoria dinamica
-    int *datito = malloc(sizeof(int));
-    int *datito2 = malloc(sizeof(int));
-    int *datito3 = malloc(sizeof(int));
-    *datito = 34;
-    *datito2 = 495;
-    *datito3 = 121;
-    cola_encolar( cola, datito);
-    cola_encolar( cola, datito2);
-    cola_encolar( cola, datito3);
-    printf("ver_primero: %d\n",*(int*)cola_ver_primero(cola) );
-    printf("desencolar: %d\n",*(int*)cola_desencolar(cola) );
-    // printf("ver_primero despues de desencolar: %d\n",*(int*)cola_ver_primero(cola) );
-
-    // encolar( cola, 45);
-    // ver_primero(cola);
-    //printf("Al principio: %d\n", cola->ultimo_en_entrar->dato);
-    //printf("Al ultimo: %d\n", cola->primero_en_entrar->dato);
-    // printf("desencolando :%d\n", desencolar(cola));
-    // printf("desencolando :%d\n", desencolar(cola));
-    // printf("desencolando :%d\n", desencolar(cola));
-
-    // destruir(cola);
-    cola_destruir(cola, free);
-    free(datito);
-    return 0;
-}
-*/
-
-
-/*
-void encolar(cola_t* cola, int dato) {
-    nodo_t *nodo = malloc(sizeof(nodo_t));
-    nodo->dato = dato;
-    nodo->siguiente = NULL;
-    if (cola->ultimo_en_entrar == NULL && cola->primero_en_entrar == NULL) {
-    cola->ultimo_en_entrar = nodo;
-    cola->primero_en_entrar = nodo;
-    return;
-    }
-    cola->ultimo_en_entrar->siguiente = nodo;
-    cola->ultimo_en_entrar = cola->ultimo_en_entrar->siguiente;
-}
-int desencolar(cola_t * cola) {
-    nodo_t * tmp = cola->primero_en_entrar;
-    int dato = tmp->dato;
-    cola->primero_en_entrar = cola->primero_en_entrar->siguiente;
     free(tmp);
-    return dato;
+    return p;
 }
-void destruir(cola_t* cola) {
-    while (cola->primero_en_entrar) {
-      desencolar(cola);
+
+void cola_destruir(cola_t *cola, void (*destruir_dato)(void *)) {
+    while (!cola_esta_vacia(cola)) {
+        if (!(destruir_dato == NULL)) {
+            destruir_dato(cola->primero->dato);
+        }
+        cola_desencolar(cola);
     }
     free(cola);
 }
-bool vacia(cola_t *cola) {
-  return cola->ultimo_en_entrar == NULL && cola->primero_en_entrar == NULL;
-}
-void esta_vacia(cola_t *cola) {
-    if (vacia(cola)) printf("esta vacia\n");
-    else printf("No esta vacia\n");
-}
-void ver_primero(const cola_t * cola) {
-    printf("el primero es: %d\n", cola->primero_en_entrar->dato);
-}
-*/
-// las primitivas para el tp
-/*
-cola_t *cola_crear(void) {
-}
-void cola_destruir(cola_t *cola, void (*destruir_dato)(void *)) {
-}
-bool cola_esta_vacia(const cola_t *cola) {
-}
-bool cola_encolar(cola_t *cola, void *valor) {
-}
-void *cola_ver_primero(const cola_t *cola) {
-}
-void *cola_desencolar(cola_t *cola) {
-}
-*/
--- pruebas_cola.c
+++ pruebas_cola.c
@@ -3,76 +3,75 @@
 #include <stddef.h>
 
 #include "cola.h"
+#include "pila.h"
 #include "testing.h"
 
 static void prueba_cola_vacia(void) {
+    printf("INICIO DE PRUEBAS DE COLA VACÍA\n");
+
     cola_t *cola = cola_crear();
-    print_test("la cola  esta vacia ", cola_esta_vacia(cola));
-    // print_test( ... pila_esta_vacia(pila) ... );
+    print_test("la cola  esta vacía ", cola_esta_vacia(cola));
+    print_test("ver primer es NULL ", cola_ver_primero(cola)==NULL);
     cola_destruir(cola, NULL);
 }
 static void prueba_cola_un_elemento_no_esta_vacia(void) {
+    printf("INICIO DE PRUEBAS DE ENCOLAR, COLA QUEDA NO VACÍA\n");
+
     cola_t *cola = cola_crear();
     int dato = 78;
     cola_encolar(cola, &dato);
-    print_test("cola con un elemento no esta vacia ", !cola_esta_vacia(cola));
-    // print_test( ... pila_esta_vacia(pila) ... );
+    print_test("cola con un elemento no esta vacía ", !cola_esta_vacia(cola));
     cola_destruir(cola, NULL);
 }
 static void prueba_cola_desencolar_elemento_queda_vacia(void) {
+    printf("INICIO DE PRUEBAS DESENCOLAR EN COLA NO VACÍA, COLA QUEDA VACÍA\n");
+
     cola_t *cola = cola_crear();
     int dato = 458;
     cola_encolar(cola, &dato);
-    int *tmp = cola_desencolar(cola);
-    // cola_desencolar(cola);
-    print_test("Acolar y desencolar 1 elemento, queda vacia: ", cola_esta_vacia(cola));
-    // print_test( ... pila_esta_vacia(pila) ... );
+    cola_desencolar(cola);
+    print_test("Acolar y desencolar 1 elemento, queda vacía: ", cola_esta_vacia(cola));
     cola_destruir(cola, NULL);
-    // free(cola);
-    free(tmp);
 }
 static void prueba_cola_ver_primero(void) {
+    printf("INICIO DE PRUEBAS DE VER PRIMERO EN COLA NO VACÍA\n");
+
     cola_t *cola = cola_crear();
     int dato = 458;
-    cola_encolar(cola, &dato);
-    print_test("prueba cola ver primero ", 458==*(int*)cola_ver_primero(cola));
+    if(cola_encolar(cola, &dato)) {
+        print_test("prueba cola ver primero ", 458==*(int*)cola_ver_primero(cola));
+    }
     cola_destruir(cola, NULL);
 }
 static void prueba_cola_desencolar_y_ver_primero(void) {
     cola_t *cola = cola_crear();
     int dato = 458;
     int dato2 = 889;
-    cola_encolar(cola, &dato);
-    cola_encolar(cola, &dato2);
-    int *tmp = cola_desencolar(cola);
-    // cola_desencolar(cola);
-    print_test("prueba cola desencolar y ver primero ", 889==*(int*)cola_ver_primero(cola));
-    free(tmp);
+    if (cola_encolar(cola, &dato)) {
+        if (cola_encolar(cola, &dato2)) {
+            cola_desencolar(cola);
+            print_test("prueba cola desencolar y ver primero ", 889==*(int*)cola_ver_primero(cola));
+        }
+    }
+
+
     cola_destruir(cola, NULL);
 }
 static void prueba_cola_encolar_null(void) {
+    printf("INICIO DE PRUEBAS DE ACOLAR NULL, LA COLA QUEDA NO VACÍA\n");
+
     cola_t *cola = cola_crear();
     cola_encolar(cola, NULL);
     print_test("cola con un NULL no esta vacia ", !cola_esta_vacia(cola));
     cola_destruir(cola, NULL);
 
 }
-void fx(void * dato) {
-  printf("funcion custom!!!\n");
-}
-static void prueba_cola_destruir_con_funcion(void) {
-    cola_t *cola = cola_crear();
-    cola_encolar(cola, NULL);
-    print_test("cola con funcion destruir custom ", !cola_esta_vacia(cola));
-    cola_destruir(cola, fx);
 
-}
-/*
 static void prueba_cola_encolar_volumen() {
     printf("INICIO DE PRUEBAS DE VOLUMEN\n");
 
     cola_t *cola = cola_crear();
-    size_t tam = 1000;
+    size_t tam = 10000;
     int datos[tam];
     for (int i = 0; i < tam; i++ ) {
         datos[i] = i;
@@ -85,48 +84,58 @@ static void prueba_cola_encolar_volumen() {
         ok &= cola_encolar(cola, &datos[i]);
     }
     print_test("se pudieron encolar todos los elementos", ok);
-    // print_test("no se puede encolar un elemento en vec_vol[tam]", !vector_guardar(vec_vol, tam, 10));
-
+    // desencolar
+    for (i = 0; i < tam; i++) {
+        // Si algun elemento no se pudo guardar correctamente, ok sera false
+        ok &= datos[i] == *(int*)cola_desencolar(cola);
+    }
+    print_test("se pudieron desencolar todos los elementos", ok);
     cola_destruir(cola, NULL);
 
 }
-*/
-/*
-static void prueba_cola_desencolar_volumen() {
-    printf("INICIO DE PRUEBAS DESENCOLAR VOLUMEN\n");
-
+void pila_destruir_wrapper(void * elem) {
+    pila_destruir(elem);
+}
+static void prueba_cola_de_pilas() {
+    printf("INICIO DE PRUEBAS COLAS DE PILAS, FUNCION DESTRUIR\n");
     cola_t *cola = cola_crear();
-    size_t tam = 100;
-    int datos[tam];
-    for (int i = 0; i < tam; i++ ) {
-        datos[i] = i;
-    }
-    //  Pruebo encolar
-    size_t i;
+    // crear dos pilas y encolarlas;
+    pila_t *pila1 = pila_crear();
+    char elementos[] = {'A','b','c', 'F','X'};
+    int tam1 = 5;
     bool ok = true;
-    for (i = 0; i < tam; i++) {
-        // Si algun elemento no se pudo guardar correctamente, ok sera false
-        ok &= cola_encolar(cola, &datos[i]);
+    for(int i=0; i < tam1; i++) {
+        pila_apilar(pila1, &elementos[i]);
+        ok &= (elementos[i]==*(char*)pila_ver_tope(pila1));
     }
-    print_test("se pudieron encolar todos los elementos", ok);
-    // pruebo desencolar
-    // int *tmp = NULL;
-    // for (i =  tam-1; i <-1; i--) {
-    //     printf("i=%lu,\n",i);
-        // Si algun elemento no se pudo guardar correctamente, ok sera false
-        // tmp = cola_desencolar(cola);
-        // printf("%lu==%d?\n", i,tmp);
-        // ok &= datos[i] == *(int*)tmp;
-        // free(tmp);
-    // }
-    // print_test("se pudieron desencolar todos los elementos", ok);
+    print_test("se pudieron apilar todos los elementos pila1", ok);
 
-    // print_test("no se puede encolar un elemento en vec_vol[tam]", !vector_guardar(vec_vol, tam, 10));
+    pila_t *pila2 = pila_crear();
+    char elementos2[] = {'G','F','D','S','B','o'};
+    int tam2 = 6;
+    ok = true;
+    for(int i=0; i < tam2; i++) {
+        pila_apilar(pila2, &elementos2[i]);
+        ok &= (elementos2[i]==*(char*)pila_ver_tope(pila2));
+    }
+    print_test("se pudieron apilar todos los elementos pila2", ok);
 
-    cola_destruir(cola, NULL);
+    cola_encolar(cola, pila1);
+    print_test("la cola  No esta vacia ", !cola_esta_vacia(cola));
+    cola_encolar(cola, pila2);
 
+    //desencolar y contrastar los valores de la pila
+    pila_t *aux = cola_desencolar(cola);
+    ok = true;
+    for(int i=0; i < tam1; i++) {
+        ok &= (elementos[tam1-i-1]==*(char*)pila_ver_tope(aux));
+        pila_desapilar(aux);
+    }
+    print_test("se pudieron desapilar todos los elementos pila1", ok);
+
+    cola_destruir(cola, pila_destruir_wrapper);
+    pila_destruir(aux);
 }
-*/
 void pruebas_cola_estudiante() {
     prueba_cola_vacia();
     prueba_cola_un_elemento_no_esta_vacia();
@@ -134,9 +143,8 @@ void pruebas_cola_estudiante() {
     prueba_cola_ver_primero();
     prueba_cola_desencolar_y_ver_primero();
     prueba_cola_encolar_null();
-    prueba_cola_destruir_con_funcion();
-    // prueba_cola_encolar_volumen();
-    // prueba_cola_desencolar_volumen();
+    prueba_cola_encolar_volumen();
+    prueba_cola_de_pilas();
 }
 
 
